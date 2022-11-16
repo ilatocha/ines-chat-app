3a391aed8d96a48c7d2bfb42dc3b9a8c2cfdd359
Ines Latocha <ines.latocha@gmail.com>
2022-11-16 13:34:38 +0100

    chat - initial app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import { Typography, Box } from "@mui/material";
+
+import MessageBox from "./components/messageBox";
+import Chat from "./components/chat";
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 0xFFFFFF).toString(16)}`;
+}
 
 function App() {
+  const [name, setName] = useState(undefined);
+  const [color, _] = useState(randomColor());
+  const title = useMemo(() => {
+    let _title = "Ines Chat App";
+    if (name) {
+      return  <>{_title}&nbsp;<b style={{color}}>{"(" + name + ")"}</b></>
+    }
+    return _title;
+  },[name, color]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Box sx={{
+      display:"flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      height: "100vh",
+    }}>
+      <header>
+        <Typography 
+          variant="h1" 
+          sx={{
+            my:2,
+            display: "flex",
+            fontSize:"20px"
+          }}>
+          {title}
+        </Typography>
       </header>
-    </div>
+      <section>
+        {name ? <Chat name={name} color={color} /> 
+              : <MessageBox onMessage={setName} hidden={name} placeholder="Enter your name and press SEND" />}
+      </section>
+    </Box>
   );
 }
 
